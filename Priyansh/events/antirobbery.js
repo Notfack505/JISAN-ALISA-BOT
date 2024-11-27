b11edb1e56fc8e6b73c3792b906d1cefc3caafe4
shehroz2006 <85061272+shehroz2006@users.noreply.github.com>
2024-11-27 23:15:37 +0500

    Update antirobbery.js

--- Priyansh/events/antirobbery.js
+++ Priyansh/events/antirobbery.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "guard",
     eventType: ["log:thread-admins"],
     version: "1.0.0",
-    credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+    credits: "ROBIN",
     description: "Prevent admin changes",
 };
 
