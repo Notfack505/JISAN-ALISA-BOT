80ba1e8ccbbbbe4d18dfb15ccabca0aa584cf347
shehroz2006 <85061272+shehroz2006@users.noreply.github.com>
2024-11-29 17:41:05 +0500

    Update bot.js

--- Priyansh/commands/bot.js
+++ Priyansh/commands/bot.js
@@ -234,7 +234,7 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
 
   if (event.body.indexOf("Bot") == 0 || (event.body.indexOf("bot") == 0)) {
     var msg = {
-      body: `${name}, ${rand}`
+      body: `${name }, ${rand}`
     }
     return api.sendMessage(msg, threadID, messageID);
   };
